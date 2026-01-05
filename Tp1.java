d7b233037a926031aed1d1edafb0c77bed4a72e0
Benoit-Monteiro-Da-Silva <benoit.monteirods@gmail.com>
2026-01-05 14:53:46 +0100

    exercice 1_2

--- Tp1.java
+++ Tp1.java
@@ -11,9 +11,11 @@ public class Tp1 {
 
         scanner = new Scanner(System.in);
 
-        exercice1_1();
+        // exercice1_1();
+        exercice1_2();
 
         scanner.close();
+
     }
 
     public static void exercice1_1() {
@@ -34,8 +36,27 @@ public class Tp1 {
         age = scanner.nextInt();
 
 		// 3° - Utilisation
-        System.out.println("Bonjour " + prenom + " " + nom);
+        System.out.println("Bonjour " + prenom + " " + nom); //Contrairement à 'print', 'println' passe à la ligne après le texte qu'on lui donne
         System.out.println("Alors comme ça vous avez " + age + " ans ?" );
+
+    }
+
+
+    public static void exercice1_2() {
+
+        int x;
+        int y;
+
+        System.out.print("Saisir valeur de x : ");
+        x = scanner.nextInt();
+        System.out.print("Saisir la valeur de y : ");
+        y = scanner.nextInt();
+
+        int storedValue = x;
+        x = y;
+        y = storedValue;
+
+        System.out.println("Les valeurs sont permutées, x vaut " + x + " et y vaut " + y);
 
     }
 
